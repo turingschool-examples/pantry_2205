a0f4d188d495a612ace93ac871e38baacd2d4f57
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-14 12:38:17 -0400

    add test for enough ingredients_for method in pantry_spec

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -1,5 +1,6 @@
 require './lib/ingredient'
 require './lib/pantry'
+require './lib/recipe'
 
 RSpec.describe Pantry do
   before(:each) do
@@ -32,23 +33,23 @@ RSpec.describe Pantry do
   it 'checks to see if it has enough ingredients for the recipe' do
     @pantry1 = Pantry.new
     @recipe1 = Recipe.new("Mac and Cheese")
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient2, 8)
-    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100 })
-    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @ingredient11 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient12 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1.add_ingredient(@ingredient11, 2)
+    @recipe1.add_ingredient(@ingredient12, 8)
+    @ingredient13 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100 })
+    @ingredient14 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe2 = Recipe.new("Cheese Burger")
-    @recipe2.add_ingredient(@ingredient1, 2)
-    @recipe2.add_ingredient(@ingredient3, 4)
-    @recipe2.add_ingredient(@ingredient4, 1)
-    @pantry.restock(@ingredient1, 5)
-    @pantry.restock(@ingredient1, 10)
-    expect(@pantry.enough_ingredients_for?(@recipe1)).to eql(false)
-    @pantry.restock(@ingredient2, 7)
-    @pantry.enough_ingredients_for?(@recipe1).to eql(false)
-    @pantry.restock(@ingredient2, 1)
-    @pantry.enough_ingredients_for?(@recipe1).to eql(true)
+    @recipe2.add_ingredient(@ingredient11, 2)
+    @recipe2.add_ingredient(@ingredient13, 4)
+    @recipe2.add_ingredient(@ingredient14, 1)
+    @pantry1.restock(@ingredient11, 5)
+    @pantry1.restock(@ingredient11, 10)
+    expect(@pantry1.enough_ingredients_for?(@recipe1)).to eql(false)
+    @pantry1.restock(@ingredient12, 7)
+    expect(@pantry1.enough_ingredients_for?(@recipe1)).to eql(false)
+    @pantry1.restock(@ingredient12, 1)
+    expect(@pantry1.enough_ingredients_for?(@recipe1)).to eql(true)
   end
 
 
