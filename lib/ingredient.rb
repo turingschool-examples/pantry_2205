5b7234370bc500bb7466fcc68633d77b7c3dd2bc
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-14 11:08:15 -0400

    Feat: return calories

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,11 +1,13 @@
 class Ingredient
 
   attr_reader :name,
-              :unit
+              :unit,
+              :calories
 
   def initialize(input)
     @name = input[:name]
     @unit = input[:unit]
+    @calories = input[:calories]
   end
 
 end
