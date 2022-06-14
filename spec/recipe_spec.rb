4d4c479863970d767eb7c99062521bfa36a3695c
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 10:20:08 -0600

    Test: Recipe can add ingredients

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe Recipe do
     expect(recipe1.name).to eq("Mac and Cheese")
     expect(recipe1.ingredients_required).to eq({})
   end
+
+  it 'can add ingredients' do
+    recipe1 = Recipe.new("Mac and Cheese")
+    expect(recipe1.ingredients_required).to eq({})
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    expect(recipe1.ingredients_required).to eq({ingredient1 => 6, ingredient2 => 8})
+  end
 end
