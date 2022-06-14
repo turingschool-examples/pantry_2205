febe66db6fd17de6d8c25900d7b4c242d6371e5a
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 12:32:37 -0400

    Add ingredients method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredient_list = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |ingredients|
+        ingredient_list << ingredients.first.name
+      end
+    end
+    ingredient_list.uniq
+  end
 end
