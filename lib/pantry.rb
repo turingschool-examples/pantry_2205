d092253a21c55b3a564f7fcb2704ea2be35a3ed9
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 09:40:05 -0600

    Feat: add stock_check method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -4,4 +4,15 @@ class Pantry
   def initialize
      @stock = {}
   end
+
+  def stock_check(ingredient)
+    # require "pry"; binding.pry
+    if @stock[ingredient.name] == nil
+      @stock[ingredient.name] = 0
+    else
+      @stock[ingredient.name]
+    end
+  end
+
+
 end
