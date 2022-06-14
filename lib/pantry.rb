edf2f4ede3e12027216c24b0f937980fe54216fc
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-14 11:16:12 -0400

    add feature: pantry.rb file

---
+++ lib/pantry.rb
@@ -0,0 +1,18 @@
+require_relative 'ingredient'
+
+class Pantry
+  attr_accessor :stock
+
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    @stock[ingredient] += amount
+  end
+
+end
