4d3dce053b8e4e9afb126850b9add29ca0f5ed77
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-14 10:52:50 -0500

    feat: ingredient spec and methods working

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,8 +1,10 @@
 class Ingredient
   attr_reader :name, :unit, :calories
   def initialize(nutrition)
+
     @name = nutrition[:name]
     @unit = nutrition[:unit]
     @calories = nutrition[:calories]
+
   end
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -2,7 +2,9 @@ require './lib/ingredient'
 
 RSpec.describe Ingredient do
   before :each do
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient1 = Ingredient.new({name:
+   "Cheese", unit: "oz", calories: 50})
+
   end
 
   it 'exists & has attributes' do
@@ -10,5 +12,6 @@ RSpec.describe Ingredient do
     expect(@ingredient1.name).to eq("Cheese")
     expect(@ingredient1.unit).to eq("oz")
     expect(@ingredient1.calories).to eq(50)
+    # require 'pry' ; binding.pry
   end
 end
