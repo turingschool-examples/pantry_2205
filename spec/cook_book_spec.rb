f28ab69a07629871f68675c154f855a79755c94e
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 12:35:53 -0400

    add highest calorie meal test and refactor

--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -13,19 +13,6 @@ RSpec.describe CookBook do
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
-	end
-
-  it 'exists' do
-    expect(@cookbook).to be_a(CookBook)
-  end
-
-  it 'can add recipes' do
-    @cookbook.add_recipe(@recipe1)
-    @cookbook.add_recipe(@recipe2)
-    expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
-  end
-
-  it 'can list all ingredients' do
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
     @recipe1.add_ingredient(@ingredient1, 2)
@@ -33,7 +20,21 @@ RSpec.describe CookBook do
     @recipe2.add_ingredient(@ingredient1, 2)
     @recipe2.add_ingredient(@ingredient3, 4)
     @recipe2.add_ingredient(@ingredient4, 1)
+	end
 
+  it 'exists' do
+    expect(@cookbook).to be_a(CookBook)
+  end
+
+  it 'can add recipes' do
+    expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
+  end
+
+  it 'can list all ingredients' do
     expect(@cookbook.ingredients).to eq(["Cheese", "Macaroni", "Ground Beef", "Bun"])
+  end
+
+  it 'can return the highest calorie meal' do
+    expect(@cookbook.highest_calorie_meal).to eq(@recipe2)
   end
 end
