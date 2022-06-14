6b43c388241c512a3c160227d5ae5ae8757e2693
Eli Sachs <easachs13@gmail.com>
2022-06-14 11:43:38 -0600

    Add It4 Cookbook tests

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -70,4 +70,40 @@ RSpec.describe CookBook do
       expect(@pantry.enough_ingredients_for?(@recipe1)).to be(true)
     end
   end
+
+  describe 'It4' do
+    before :each do
+      @cookbook = CookBook.new
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 1})
+      @recipe2 = Recipe.new("Burger")
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 100)
+      @cookbook.add_recipe(@recipe1)
+      @cookbook.add_recipe(@recipe2)
+      @expected_sum = [{
+        :name=>"Mac and Cheese",
+        :details=>{:ingredients=>[{:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}],
+        :total_calories=>440}}, {
+        :name=>"Burger",
+        :details=>{:ingredients=>[{:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}],
+        :total_calories=>500}}]
+    end
+
+    it 'has a date' do
+      expect(@cookbook.date).to be_a(String)
+      expect(@cookbook.date[0..2]).to eq('06-')
+      expect(@cookbook.date[-5..-1]).to eq('-2022')
+    end
+
+    it 'has a summary' do
+      # require "pry"; binding.pry
+      expect(@cookbook.summary).to eq(@expected_sum)
+    end
+  end
 end
