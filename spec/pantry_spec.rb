67f5542cf34dc650d52ca1bf11c4553569cd6804
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 09:47:48 -0600

    Test: made restock ingredients test more robust

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -31,6 +31,8 @@ RSpec.describe Pantry do
   it 'can restock ingredients' do
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
+    @pantry.restock(@ingredient2, 7)
     expect(@pantry.stock_check(@ingredient1)).to eq(15)
+    expect(@pantry.stock_check(@ingredient2)).to eq(7)
   end
 end
