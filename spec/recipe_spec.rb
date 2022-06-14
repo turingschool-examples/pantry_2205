1d574eb0a32fc11f496042e4013cbf5505ca3b3e
Brenna Stuart <brennacodes@gmail.com>
2022-06-14 09:27:52 -0600

    Test: add recipe spec

---
+++ spec/recipe_spec.rb
@@ -0,0 +1,10 @@
+require './lib/helper'
+
+RSpec.describe 'Recipe' do
+    let!(:cheese) { Ingredient.new({name: "Cheese", unit: "oz", calories: 50}) }
+    let!(:macaroni) { Ingredient.new({name: "Macaroni", unit: "oz", calories: 200}) }
+    let!(:mac) { Recipe.new("Mac and Cheese") }
+
+    it 'exists' do
+        expect(mac).to be_instance_of(Recipe)
+    end
