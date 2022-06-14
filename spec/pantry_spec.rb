207e5b32037c92965d8925c08732149aaf246eeb
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 09:46:57 -0600

    Test: Pantry can check stock

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Pantry do
     pantry = Pantry.new
     expect(pantry.stock).to eq({})
   end
+
+  it 'can check stock' do
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    expect(pantry.stock_check(ingredient1)).to eq(0)
+    expect(pantry.stock_check(ingredient2)).to eq(0)
+  end
 end
