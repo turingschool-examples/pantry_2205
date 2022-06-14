4102a001e03e5c262b7148f6f7df56f12746dc98
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 10:42:41 -0600

    Test: Cook book can add recipes

--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe CookBook do
     cookbook = CookBook.new
     expect(cookbook.recipes).to eq([])
   end
+
+  it 'can add recipes' do
+    cookbook = CookBook.new
+    expect(cookbook.recipes).to eq([])
+
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    expect(cookbook.recipes).to eq([recipe1, recipe2])
+  end
 end
