fd6c3aa0e63e8de5d7c93daf2667db9780438655
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 10:15:45 -0600

    Feat: create add_ingredient method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,4 +6,12 @@ class Recipe
     @name = name
     @ingredients_required = {}
   end
+
+  def add_ingredient(ingredient, number)
+    if @ingredients_required[ingredient] == nil
+      @ingredients_required[ingredient] = number
+    else
+      @ingredients_required[ingredient] += number
+    end
+  end
 end
