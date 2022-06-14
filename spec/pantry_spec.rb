8f9d65f7d51535925285aae52ed04027f3d4484c
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-06-14 09:19:52 -0600

    iteration 1

---
+++ spec/pantry_spec.rb
@@ -0,0 +1,27 @@
+require './lib/ingredient'
+require './lib/pantry'
+
+RSpec.describe Pantry do
+  before(:each) do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @pantry = Pantry.new
+  end
+
+  it 'exists and has no stock' do
+    expect(@pantry).to be_a(Pantry)
+    expect(@pantry.stock).to eq({})
+  end
+
+  it 'can check for ingredient stock' do
+    expect(@pantry.stock_check(@ingredient1)).to eq(0)
+  end
+
+  it 'can restock ingredients and amounts' do
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    expect(@pantry.stock_check(@ingredient1)).to eq(15)
+    @pantry.restock(@ingredient2, 7)
+    expect(@pantry.stock_check(@ingredient2)).to eq(7)
+  end
+end
