c01c637df63943b90495ca450ba5874f9206ed17
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:59:07 -0600

    Create method to return all ingredients in a cookbook

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -10,4 +10,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredients = []
+    @recipes.each do |recipe|
+      recipe.ingredients.each do |ingredient|
+        ingredients << ingredient.name
+      end
+    end
+    ingredients.uniq!
+  end
 end
