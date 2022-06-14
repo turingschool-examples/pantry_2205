06f6185553bc669ae28bbfe76e15c46bf760899e
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 11:08:20 -0400

    Add tests

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -2,25 +2,20 @@ require 'rspec'
 require './lib/ingredient'
 
 RSpec.describe Ingredient do
+  it 'exists' do
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    expect(ingredient1).to be_a(Ingredient)
+  end
 
-  pry(main)> require './lib/ingredient'
-  # => true
+  it 'has attributes' do
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient1.name
+    expect(ingredient1.name).to eq("Cheese")
 
-  pry(main)> require './lib/pantry'
-  # => true
+    ingredient1.unit
+    expect(ingredient1.unit).to eq("oz")
 
-  pry(main)> ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-  # => #<Ingredient:0x007fe6041273d8...>
-
-  pry(main)> ingredient1.name
-  # => "Cheese"
-
-  pry(main)> ingredient1.unit
-  # => "oz"
-
-  pry(main)> ingredient1.calories
-  # => 50
-
-  pry(main)> ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
-  # => #<Ingredient:0x007fd88582ed98...>
+    ingredient1.calories
+    expect(ingredient1.calories).to eq(50)
+  end
 end
