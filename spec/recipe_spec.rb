1fea26c7a11a48f21be75afc04f89a0d5cba3b95
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 10:06:43 -0600

    Test: Recipe has attributes

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Recipe do
     recipe1 = Recipe.new("Mac and Cheese")
     expect(recipe1).to be_a(Recipe)
   end
+
+  it 'has attributes' do
+    recipe1 = Recipe.new("Mac and Cheese")
+    expect(recipe1.name).to eq("Mac and Cheese")
+    expect(recipe1.ingredients_required).to eq({})
+  end
 end
