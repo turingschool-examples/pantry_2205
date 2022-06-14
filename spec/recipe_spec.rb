d1ac29415d6b88651d665812ac42ce6135695bc8
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:49:10 -0600

    Add test for return count of calories in a recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -4,8 +4,11 @@ RSpec.describe Recipe do
   before :each do
     @ingredient1 = Ingredient.new({name: 'Cheese', unit: 'C', calories: 100})
     @ingredient2 = Ingredient.new({name: 'Macaroni', unit: 'oz', calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new('Mac and Cheese')
-    recipe2 = Recipe.new("Cheese Burger")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @recipe3 = Recipe.new("Test Recipe")
   end
 
   describe '#initialize' do
@@ -42,6 +45,19 @@ RSpec.describe Recipe do
       expect(@recipe1.ingredients).to eq [@ingredient1]
       @recipe1.add_ingredient(@ingredient2, 8)
       expect(@recipe1.ingredients).to eq [@ingredient1, @ingredient2]
+    end
+  end
+
+  describe '#total_calories' do
+    it 'can return an integer of the count of calories in a recipe' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expect(@recipe1.total_calories).to eq 440
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+      expect(@recipe2.total_calories).to eq 675
+      expect(@recipe3.total_calories).to eq 0
     end
   end
 end
