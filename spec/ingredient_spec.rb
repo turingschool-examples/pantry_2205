3123a5daaa84813aa9fc2b0d1404ab3bbf685dee
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-14 11:05:05 -0400

    add tests: ingredient_spec

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,8 +1,24 @@
 require 'ingredient'
 
-RSpec.describe Ingredient
+RSpec.describe Ingredient do
   before(:each) do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+  end
+
+  it 'exists' do
+    expect(@ingredient1).to be_a(Ingredient)
+  end
+
+  it 'has a name' do
+    expect(@ingredient1.name).to eql("Cheese")
+  end
+
+  it 'has a unit' do
+    expect(@ingredient1.unit).to eql("oz")
+  end
+
+  it 'has calories' do
+    expect(@ingredient1.calories).to eql(50)
   end
 
 end
