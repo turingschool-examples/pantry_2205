cb70a398fceca978b4c8251fa14cf5cd717c479d
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:30:12 -0600

    Create method to add ingredients to ingredients required hash

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -7,4 +7,12 @@ class Recipe
     @name = name
     @ingredients_required = {}
   end
+
+  def add_ingredient(ingredient, quantity)
+    if @ingredients_required.keys.include?(ingredient)
+      @ingredients_required[ingredient] += quantity
+    else
+      @ingredients_required[ingredient] = quantity
+    end
+  end
 end
