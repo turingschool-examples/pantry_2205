2e13df0b35cddb85e774bef6b835fd49e1f8884c
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:49:20 -0600

    Create method to return count of calories in a recipe

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -19,4 +19,12 @@ class Recipe
   def ingredients
     @ingredients_required.keys
   end
+
+  def total_calories
+    calories = 0
+    @ingredients_required.each do |ingredient, quantity|
+      calories += quantity * ingredient.calories
+    end
+    calories
+  end
 end
