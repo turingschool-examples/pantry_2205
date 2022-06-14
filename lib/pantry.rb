85ea895ec1c17e2da113d4c149c97f8f21b32bbb
Eli Sachs <easachs13@gmail.com>
2022-06-14 09:14:48 -0600

    Add Pantry class

---
+++ lib/pantry.rb
@@ -0,0 +1,17 @@
+class Pantry
+
+  attr_reader :stock
+
+  def initialize
+    @stock = Hash.new(0)
+  end
+
+  def stock_check(ingredient)
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amt)
+    @stock[ingredient] += amt
+  end
+
+end
