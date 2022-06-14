4a597ad3888af4a672ef6f4c23329ae6fb56edfc
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 11:58:18 -0600

    Feat: start sudocode for enough ingredients for method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -20,4 +20,12 @@ class Pantry
       @stock[ingredient] += number
     end
   end
+
+  def enough_ingredients_for?(recipe)
+    require "pry"; binding.pry
+    required_values = recipe.ingredients_required.values
+    ingredients = recipe.ingredients
+    #enumerate over required recipe items to be sure values of
+    # pantry items is >= required recipe items. out of time
+  end
 end
