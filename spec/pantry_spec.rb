c0c6c7c4314dd25e6dc7aa706eeca4221ddb627d
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 09:38:52 -0600

    Test: Pantry has a stock

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Pantry do
     pantry = Pantry.new
     expect(pantry).to be_a (Pantry)
   end
+
+  it 'has a stock' do
+    pantry = Pantry.new
+    expect(pantry.stock).to eq({})
+  end
 end
