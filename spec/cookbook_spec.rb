43d4f7da4505a3bea1f5e18dac265a5ef24c6af5
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:58:52 -0600

    Create test for return array of ingredients in a cookbook

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe CookBook do
     @cookbook = CookBook.new
     @ingredient1 = Ingredient.new({name: 'Cheese', unit: 'C', calories: 100})
     @ingredient2 = Ingredient.new({name: 'Macaroni', unit: 'oz', calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new('Mac and Cheese')
     @recipe2 = Recipe.new("Cheese Burger")
+    @recipe3 = Recipe.new("Test Recipe")
   end
 
   describe '#initialize' do
@@ -25,6 +28,20 @@ RSpec.describe CookBook do
       expect(@cookbook.recipes).to eq [@recipe1]
       @cookbook.add_recipe(@recipe2)
       expect(@cookbook.recipes).to eq [@recipe1, @recipe2]
+    end
+  end
+
+  describe '#ingredients' do
+    it 'returns an array of all ingredient names within recipes in a cookbook' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+      @cookbook.add_recipe(@recipe1)
+      @cookbook.add_recipe(@recipe2)
+      expected_array = ['Cheese', 'Macaroni', 'Ground Beef', 'Bun']
+      expect(@cookbook.ingredients).to eq expected_array
     end
   end
 end
