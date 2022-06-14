fa23ec2c337c410f23616136bd4d8ecf142741bc
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 11:55:00 -0400

    Create file and add tests

---
+++ spec/cook_book_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require './lib/pantry'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/cookbook'
+
+RSpec.describe CookBook do
+	before :each do
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @cookbook = CookBook.new
+	end
+
+  it 'exists' do
+    expect(@cookbook).to be_a(CookBook)
+  end
+
+  it 'can add recipes' do
+    cookbook.add_recipe(@recipe1)
+    cookbook.add_recipe(@recipe2)
+    expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
+  end
+end
