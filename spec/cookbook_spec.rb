bd2c2ae3f1bd4cac3f12a9a860785af2ff42d72b
Eli Sachs <easachs13@gmail.com>
2022-06-14 09:37:26 -0600

    Recipe feat total calories (spec)

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -1,26 +1,52 @@
 require './lib/ingredient'
 require './lib/recipe'
 require './lib/cookbook'
+require './lib/pantry'
 RSpec.describe CookBook do
-  before :each do
-    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
-    @recipe1 = Recipe.new("Mac and Cheese")
-    @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient1, 4)
-    @recipe1.add_ingredient(@ingredient2, 8)
-    @recipe2 = Recipe.new("Cheese Burger")
-    @cookbook = CookBook.new
+  describe '#It2' do
+    before :each do
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient1, 4)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @recipe2 = Recipe.new("Cheese Burger")
+      @cookbook = CookBook.new
+    end
+
+    it 'exists' do
+      expect(@cookbook).to be_a(CookBook)
+      expect(@cookbook.recipes).to eq([])
+    end
+
+    it 'can add recipes' do
+      @cookbook.add_recipe(@recipe1)
+      @cookbook.add_recipe(@recipe2)
+      expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
+    end
   end
 
-  it 'exists' do
-    expect(@cookbook).to be_a(CookBook)
-    expect(@cookbook.recipes).to eq([])
-  end
+  describe 'It3' do
+    before :each do
+      @pantry = Pantry.new
+      @cookbook = CookBook.new
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+      @recipe1 = Recipe.new("Mac and Cheese")
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+      @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      @recipe2 = Recipe.new("Cheese Burger")
+      @recipe2.add_ingredient(@ingredient1, 2)
+      @recipe2.add_ingredient(@ingredient3, 4)
+      @recipe2.add_ingredient(@ingredient4, 1)
+    end
 
-  it 'can add recipes' do
-    @cookbook.add_recipe(@recipe1)
-    @cookbook.add_recipe(@recipe2)
-    expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
+    it 'recipes can show total calories' do
+      expect(@recipe1.total_calories).to eq(440)
+      expect(@recipe2.total_calories).to eq(675)
+    end
   end
 end
