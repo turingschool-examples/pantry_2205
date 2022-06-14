83c9bca0c10768f035592cfb40c57ddeb73b1123
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:07:37 -0600

    Create spec and add initialization tests for Pantry

---
+++ spec/pantry_spec.rb
@@ -0,0 +1,29 @@
+require './lib/pantry'
+
+RSpec.describe Pantry do
+  before :each do
+    @ingredient1 = Ingredient.new(
+      { name: "Cheese",
+        unit: "oz",
+        calories: 50
+      }
+    )
+    @ingredient2 = Ingredient.new(
+      { name: "Macaroni",
+        unit: "oz",
+        calories: 200
+      }
+    )
+    @pantry = Pantry.new
+  end
+
+  describe '#initialize' do
+    it 'is a Pantry' do
+      expect(@pantry).to be_a Pantry
+    end
+
+    it 'initializes with an empty hash of stock' do
+      expect(@pantry.stock).to eq {}
+    end
+  end
+end
