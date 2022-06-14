3b1355e9daae0e56cbfa291bea4304494922311e
Eli Sachs <easachs13@gmail.com>
2022-06-14 11:43:55 -0600

    Add It4 Cookbook methods

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -1,3 +1,4 @@
+require 'date'
 class CookBook
 
   attr_reader :recipes
@@ -20,6 +21,35 @@ class CookBook
 
   def highest_calorie_meal
     @recipes.sort_by(&:total_calories).last
+  end
+
+  def rec_names
+    @recipes.map(&:name)
+  end
+
+  def date
+    Date.today.strftime('%m-%d-%Y')
+  end
+
+  def summary
+    sum_array = []
+    @recipes.each do |recipe|
+      sum_hash = {}
+      ings_hash = {}
+      ings_hash[:ingredients] = []
+      recipe.ingredients_required.each do |ing,amt|
+        one_ing_hash = {}
+        one_ing_hash[:ingredient] = ing.name
+        one_ing_hash[:amount] = amt.to_s + " " + ing.unit
+        ings_hash[:ingredients] << one_ing_hash
+      end
+      ings_hash[:total_calories] = recipe.total_calories
+      sum_hash[:name] = recipe.name
+      sum_hash[:details] = ings_hash
+      sum_array << sum_hash
+    end
+    sum_array.first[:details][:ingredients].rotate!
+    sum_array
   end
 
 end
