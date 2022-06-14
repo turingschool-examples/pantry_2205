776876e14afd4e601665773e4e2f31c46372a830
Kevin Ta <kvnta@yahoo.com>
2022-06-14 09:22:14 -0600

    Test: has attributes

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -10,3 +10,11 @@ RSpec.describe Ingredient do
     expect(@ingredient1).to be_a(Ingredient)
     expect(@ingredient2).to be_a(Ingredient)
   end
+  it 'has attributes' do
+    expect(@ingredient1.name).to eq("Cheese")
+    expect(@ingredient1.unit). to eq("oz")
+    expect(@ingredient1.calories).to eq(50)
+  end
+
+
+end
