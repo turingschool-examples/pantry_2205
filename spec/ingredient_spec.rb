531daa1e99a9930dfb371ef44526e339e376b8ac
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-14 10:21:19 -0500

    test: initital iteration pattern tests for party

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -3,7 +3,6 @@ require './lib/ingredient'
 RSpec.describe Ingredient do
   before :each do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   it 'exists & has attributes' do
---
+++ spec/patry_spec.rb
@@ -0,0 +1,26 @@
+require './lib/pantry'
+require './lib/ingredient'
+
+RSpec.describe Pantry do
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @pantry = Pantry.new
+  end
+
+  it 'exists' do
+    expect(@pantry).to be_a(Pantry)
+  end
+
+  it "can stock, restock, and stock check ingredients" do
+    @pantry.stock #=> {}
+    # @pantry.stock_check(@ingredient1) #=> 0
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+
+    @pantry.restock(@ingredient2, 7) #=> 15
+
+    expect(@pantry.stock_check(@ingredient1)).to eq(15)
+    expect(@pantry.stock_check(@ingredient2)).to eq(7)
+  end
+end
