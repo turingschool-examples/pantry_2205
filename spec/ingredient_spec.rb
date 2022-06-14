1d52ce4833ac2b3cfa7b5d637bde20a8f1700e08
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 09:20:00 -0600

    Test: Ingredient exists

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,5 +1,9 @@
 require './lib/ingredient'
 
 RSpec.describe Ingredient do
+  it 'exists' do
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    expect(ingredient1).to be_a(Ingredient)
+  end
 
 end
