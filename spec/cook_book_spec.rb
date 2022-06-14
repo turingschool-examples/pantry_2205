479099c5bc8f3cbc5ab6a8747337889d4cabe6b8
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 12:32:08 -0400

    Add ingredients to file and to each recipe

--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -6,6 +6,10 @@ require './lib/cook_book'
 
 RSpec.describe CookBook do
 	before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+		@ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
     @cookbook = CookBook.new
@@ -24,6 +28,11 @@ RSpec.describe CookBook do
   it 'can list all ingredients' do
     @cookbook.add_recipe(@recipe1)
     @cookbook.add_recipe(@recipe2)
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
 
     expect(@cookbook.ingredients).to eq(["Cheese", "Macaroni", "Ground Beef", "Bun"])
   end
