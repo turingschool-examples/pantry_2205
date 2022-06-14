6af43da2f6c50931a3b6913f55b375421b4da0e2
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 09:46:59 -0600

    Feat: restock and stock methods complete

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -2,10 +2,23 @@ class Pantry
   attr_reader :stock
 
   def initialize
-    @stock = {}
+    @stock = Hash.new(0)
   end
 
   def stock_check(ingredient)
-    return 0 if @stock == {} 
+    return 0 if @stock == {}
+    if @stock.include?(ingredient)
+      @stock[ingredient]
+    else
+      0
+    end
+  end
+
+  def restock(ingredient, quantity)
+    if @stock.include?(ingredient) == false
+      @stock[ingredient] = quantity
+    else
+      @stock[ingredient] += quantity
+    end
   end
 end
