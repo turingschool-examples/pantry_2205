8c66282a3f384c211cdede647d66430e0241fca1
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 10:17:56 -0600

    Test: can add recipes

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -28,4 +28,10 @@ RSpec.describe CookBook do
   it 'has a recipes array' do
     expect(@cookbook.recipes).to eq([])
   end
+
+  it 'can add recipes' do
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
+  end
 end
