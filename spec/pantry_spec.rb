549d13581e066954dadf73314ff26910677e10eb
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 11:14:32 -0600

    Create test for if there are enough ingredients for a recipe

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -2,18 +2,12 @@ require './lib/pantry'
 
 RSpec.describe Pantry do
   before :each do
-    @ingredient1 = Ingredient.new(
-      { name: 'Cheese',
-        unit: 'oz',
-        calories: 50
-      }
-    )
-    @ingredient2 = Ingredient.new(
-      { name: 'Macaroni',
-        unit: 'oz',
-        calories: 200
-      }
-    )
+    @ingredient1 = Ingredient.new({name: 'Cheese', unit: 'oz', calories: 50})
+    @ingredient2 = Ingredient.new({name: 'Macaroni', unit: 'oz', calories: 200})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
     @pantry = Pantry.new
   end
 
@@ -44,6 +38,26 @@ RSpec.describe Pantry do
       expect(@pantry.stock).to eq ({ @ingredient1 => 15})
       @pantry.restock(@ingredient2, 7)
       expect(@pantry.stock).to eq ({ @ingredient1 => 15, @ingredient2 => 7})
+    end
+  end
+
+  describe '#enough_ingredients_for' do
+    it 'returns false if there are not enough ingredients for a recipe' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to eq false
+    end
+
+    it 'returns true if there are enough ingredients for a recipe' do
+      @recipe1.add_ingredient(@ingredient1, 2)
+      @recipe1.add_ingredient(@ingredient2, 8)
+      @pantry.restock(@ingredient1, 1)
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to eq false
+      @pantry.restock(@ingredient1, 1)
+      @pantry.restock(@ingredient2, 8)
+      expect(@pantry.enough_ingredients_for?(@recipe1)).to eq true
+      @recipe2.add_ingredient(@ingredient3, 5)
+      expect(@pantry.enough_ingredients_for?(@recipe2)).to eq false
     end
   end
 end
