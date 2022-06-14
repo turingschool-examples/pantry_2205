ab46ada411331e464a87a41400983e7830bdb43e
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 11:14:02 -0600

    Test: write ingredients test

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -24,5 +24,21 @@ RSpec.describe CookBook do
 
       expect(@cookbook.recipes).to eq([@recipe1, @recipe2])
     end
+
+    describe 'iteration 3' do
+      before :each do
+        @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+        @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+        @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+        @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+      end
+
+      it "can tell all ingredients in recipes" do
+        @cookbook.add_recipe(@recipe1)
+        @cookbook.add_recipe(@recipe2)
+
+        expect(@cookbook.ingredients).to eq(["Cheese", "Macaroni", "Ground Beef", "Bun"])
+      end
+    end
 	end
 end
