1d03c97e655054fc2b8888386b5b910b9545f7e6
Eli Sachs <easachs13@gmail.com>
2022-06-14 09:45:11 -0600

    Cookbook feat ingredients (class)

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -10,4 +10,16 @@ class CookBook
     @recipes << recipe
   end
 
+  def ingredients
+    all_ing = []
+    @recipes.each do |recipe|
+      all_ing << recipe.ingredients
+    end
+    all_ing.flatten.map(&:name).uniq
+  end
+
+  # def highest_calorie_meal
+  # 
+  # end
+
 end
