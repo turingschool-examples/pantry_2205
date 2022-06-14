6d911c547a67f688bcb2d8d38ef9d5fb3ad721b6
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 09:54:19 -0600

    Feat: Pass test pantry restocks

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -7,6 +7,16 @@ class Pantry
   def stock_check(ingredient)
     if @stock[ingredient].nil?
       0
+    else
+      @stock[ingredient]
+    end
+  end
+
+  def restock(ingredient, amount)
+    if @stock[ingredient].nil?
+      @stock[ingredient] = amount
+    else
+      @stock[ingredient] += amount
     end
   end
 end
