79c6c7f03969d6c7c18a84e4ea1480979048019c
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-14 11:23:45 -0400

    Test: recipe exists

---
+++ spec/recipe_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+
+RSpec.describe Recipe do
+
+  before(:each) do
+    @ingredient1 = Ingredient.new({
+      name: "Cheese",
+      unit: "oz",
+      calories: 50})
+
+    @ingredient2 = Ingredient.new({
+      name: "Macaroni",
+      unit: "oz",
+      calories: 200})
+
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
+
+  it 'exists' do
+    expect(@recipe1).to be_instance_of(Recipe)
+  end
+
+end
