ce2e8a3a17cea323cb9d898d068f5f57bbdd9b04
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 12:08:24 -0400

    Add total calories method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -12,4 +12,10 @@ class Recipe
   def ingredients
     @ingredients_required.keys
   end
+
+  def total_calories
+    @ingredients_required.sum do |ingredient, qty|
+      ingredient.calories * qty
+    end
+  end
 end
