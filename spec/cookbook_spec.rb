a0ca7aaafbb6c9875fcb948cb66625edf2021ff7
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-14 11:56:10 -0400

    add test: ingredients method cookbook.rb

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -24,4 +24,22 @@ RSpec.describe CookBook do
     expect(@cookbook.recipes).to eql([@recipe6, @recipe7])
   end
 
+  it 'finds the ingredients of the cookbook' do
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100 })
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    @recipe2 = Recipe.new("Cheese Burger")
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    expect(@cookbook.ingredients).to eql(["Cheese", "Macaroni", "Ground Beef", "Bun"])
+  end
+
+
 end
