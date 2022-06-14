d39188dfa294a1e434fd0440c1c7a3d0b178b0bd
Brenna Stuart <brennacodes@gmail.com>
2022-06-14 09:07:04 -0600

    Test: add ingredient exists

---
+++ lib/ingredient.rb
@@ -0,0 +1,9 @@
+require './lib/helper'
+
+RSpec.describe 'Ingredient' do
+    let!(:cheese) { Ingredient.new({name: "Cheese", unit: "oz", calories: 50}) }
+    
+    it 'exists' do
+        expect(cheese).to be_instance_of(Ingredient)
+    end
+end
