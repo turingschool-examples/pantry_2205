95a5e99035877f3fe3f6de520e4a291e95f36e5d
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 11:07:15 -0600

    Feat: ingredients method complete

--- lib/cookbook.rb
+++ lib/cookbook.rb
@@ -8,4 +8,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredient_names = []
+    @recipes.each do |recipe_instance|
+      recipe_instance.ingredients_required.keys.each do |ingredient_instance|
+        ingredient_names << ingredient_instance.name
+      end
+    end
+    ingredient_names.uniq
+  end
 end
