6e34e4c9037302f5222a4fcc609768890395d366
Riley McCullough <rileybmccullough@gmail.com>
2022-06-14 11:50:34 -0600

    Test: can find ingredients for recipes

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe CookBook do
     expect(cookbook.recipes).to eq(["Mac and Cheese", "Cheese Burger"])
   end
 
+  it 'can find ingredients for multiple recipes' do
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook = CookBook.new
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    expect(cookbook.ingredients).to eq(["Cheese", "Macaroni", "Ground Beef", "Bun"])
+
+  end
+
 end
