1578a259d8ba8a7de4441d3cb40ef3f72bb5e7c9
Brenna Stuart <brennacodes@gmail.com>
2022-06-14 09:11:36 -0600

    Test: ingredient has a name/unit/calories; pass

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe 'Ingredient' do
     it 'has a name' do
         expect(cheese.name).to eq("Cheese")
     end
+
+    it 'has a unit' do
+        expect(cheese.unit).to eq("oz")
+    end
+
+    it 'has calories' do
+        expect(cheese.calories).to eq(50)
+    end
 end
