9558db84fd4f2ce1bc00c1a11e67a658577a8b37
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-06-14 10:13:13 -0500

    test: intial iteration pattern tests for ingredient written

---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,15 @@
+require './lib/ingredient'
+
+RSpec.describe Ingredient do
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+  end
+
+  it 'exists & has attributes' do
+    expect(@ingredient1).to be_a(Ingredient)
+    expect(@ingredient1.name).to eq("Cheese")
+    expect(@ingredient1.unit).to eq("oz")
+    expect(@ingredient1.calories).to eq(50)
+  end
+end
