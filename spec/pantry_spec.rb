c3fde840425e0a858cf596d2ab1d5ac467535d56
Kevin Ta <kvnta@yahoo.com>
2022-06-14 09:50:11 -0600

    Test: Pantry can check stock and restock ingredients

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Pantry do
     expect(@pantry.stock).to eq({})
   end
 
-  
+  it 'can check stock and restock ingredients' do
+    expect(@pantry.stock_check(@ingredient1)).to eq(0)
+    @pantry.restock(@ingredient1, 5)
+    expect(@pantry.stock).to eq({@ingredient1 => 5})
+    expect(@pantry.stock_check(@ingredient1)).to eq(5)
+    @pantry.restock(@ingredient1, 10)
+    expect(@pantry.stock_check(@ingredient1)).to eq(15)
+
+    @pantry.restock(@ingredient2, 7)
+    expect(@pantry.stock_check(@ingredient2)).to eq(7)
+  end
+end
