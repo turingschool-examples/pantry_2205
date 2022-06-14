3c47d275695714bad8d76f6010cae70ba23e2ce7
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 09:20:40 -0600

    Test: create basic tests for pantry

---
+++ spec/pantry_spec.rb
@@ -0,0 +1,21 @@
+require "rspec"
+require "./lib/ingredient"
+require "./lib/pantry"
+
+RSpec.describe Pantry do
+	describe "Iteration 2" do
+		before :each do
+      @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+      @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+      @pantry = Pantry.new
+    end
+
+		it "exists" do
+			expect(@pantry).to be_a(Pantry)
+		end
+
+		it "can start with an empty stock hash" do
+			expect(@pantry.stock).to eq({})
+    end
+	end
+end
