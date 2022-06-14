3a4a39cf20a52a0b5f25cd920885c7350379fa65
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 10:15:24 -0600

    Debug: camelcase cookbook to CookBook

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -3,7 +3,7 @@ require './lib/pantry'
 require './lib/recipe'
 require './lib/cookbook'
 
-RSpec.describe Cookbook do
+RSpec.describe CookBook do
   before :each do
     @ingredient1 = Ingredient.new({
       name: "Cheese",
@@ -18,10 +18,10 @@ RSpec.describe Cookbook do
     @pantry = Pantry.new
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
-    cookbook = CookBook.new
+    @cookbook = CookBook.new
   end
 
   it 'exists' do
-    expect(@cookbook).to be_a(Cookbook)
+    expect(@cookbook).to be_a(CookBook)
   end
 end
