1ee2df6beed0f7a16c7404014643afcb25309e61
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 09:53:39 -0600

    Test: Pantry can restock

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe Pantry do
     expect(pantry.stock_check(ingredient1)).to eq(0)
     expect(pantry.stock_check(ingredient2)).to eq(0)
   end
+
+  it 'can restock' do
+    pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    expect(pantry.stock_check(ingredient1)).to eq(0)
+    expect(pantry.stock_check(ingredient2)).to eq(0)
+
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+    expect(pantry.stock_check(ingredient1)).to eq(15)
+
+    pantry.restock(ingredient2, 7)
+    expect(pantry.stock_check(ingredient2)).to eq(7)
+  end
 end
