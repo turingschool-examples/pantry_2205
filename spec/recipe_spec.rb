6eecd8667a9042dc71af7c83c45fa73381f22cd2
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 10:51:21 -0600

    Test: Recipe can return total calories from ingredients

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -40,4 +40,22 @@ RSpec.describe Recipe do
 
     expect(recipe1.ingredients).to eq([ingredient1, ingredient2])
   end
+
+  it 'can return total calories from ingredients' do
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    expect(recipe1.total_calories).to eq(440)
+    expect(recipe2.total_calories).to eq(675)
+  end
 end
