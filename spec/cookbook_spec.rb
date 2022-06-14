63f120ccb4bcb71d397d0b2964f3c0ef7ce8a011
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 10:12:47 -0600

    Test: it exists

---
+++ spec/cookbook_spec.rb
@@ -0,0 +1,27 @@
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cookbook'
+
+RSpec.describe Cookbook do
+  before :each do
+    @ingredient1 = Ingredient.new({
+      name: "Cheese",
+      unit: "C",
+      calories: 100
+      })
+    @ingredient2 = Ingredient.new({
+      name: "Macaroni",
+      unit: "oz",
+      calories: 30
+      })
+    @pantry = Pantry.new
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    cookbook = CookBook.new
+  end
+
+  it 'exists' do
+    expect(@cookbook).to be_a(Cookbook)
+  end
+end
