d30178bb8f5f85ddb3d8e547136f20577716a68e
Kevin Ta <kvnta@yahoo.com>
2022-06-14 09:21:49 -0600

    Test: it exists

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -1,2 +1,12 @@
 require './lib/ingredient'
 require './lib/pantry'
+RSpec.describe Ingredient do
+  before :each do
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+  end
+
+  it 'exists' do
+    expect(@ingredient1).to be_a(Ingredient)
+    expect(@ingredient2).to be_a(Ingredient)
+  end
