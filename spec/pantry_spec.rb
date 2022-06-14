831f08e528c6960ad3f1f2b860239ae7d3872c46
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-14 11:10:55 -0400

    Test: pantry exists

---
+++ spec/pantry_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/ingredient'
+require './lib/pantry'
+
+RSpec.describe Pantry do
+
+  before(:each) do
+    @ingredient1 = Ingredient.new({
+      name: "Cheese",
+      unit: "oz",
+      calories: 50})
+
+    @ingredient2 = Ingredient.new({
+      name: "Macaroni",
+      unit: "oz",
+      calories: 200})
+
+    @pantry = Pantry.new
+  end
+
+  it 'exists' do
+    expect(@pantry).to be_instance_of(Pantry)
+  end
+
+end
