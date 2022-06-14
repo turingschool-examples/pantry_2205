d1c73b8167ed6253f220c83077c3780a0c445da5
Lucas Colwell <colwellc37@gmail.com>
2022-06-14 09:12:14 -0600

    Test: create tests for ingredients

---
+++ spec/ingredient_spec.rb
@@ -0,0 +1,24 @@
+require "rspec"
+require "./lib/ingredient"
+
+RSpec.describe Class do
+	describe "Iteration 1" do
+		before :each do
+			@ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+			@ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+		end
+
+		it "exists" do
+			expect(@ingredient1).to be_a(Ingredient)
+		end
+
+		it "has attributes" do
+			expect(@ingredient1.name).to eq("Cheese")
+      expect(@ingredient1.unit).to eq("oz")
+      expect(@ingredient1.calories).to eq(50)
+      expect(@ingredient1.name).to eq("Macaroni")
+      expect(@ingredient1.unit).to eq("oz")
+      expect(@ingredient1.calories).to eq(200)
+    end
+	end
+end
