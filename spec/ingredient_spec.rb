22957860aa8b9331b685dcf83a3dd12b6b3b84e4
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 09:28:26 -0600

    Test: Ingredient has attributes

--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Ingredient do
     expect(ingredient1).to be_a(Ingredient)
   end
 
+  it 'has attributes' do
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    expect(ingredient1.name).to eq("Cheese")
+    expect(ingredient1.unit).to eq("oz")
+    expect(ingredient1.calories).to eq(50)
+  end
 end
