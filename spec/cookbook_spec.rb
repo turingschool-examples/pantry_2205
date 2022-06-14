9ebea04818024c5a017ddb6cd5890c21e967f921
Blake Saylor <blakesaylor@gmail.com>
2022-06-14 09:36:59 -0600

    Add test for adding a recipe to cookbook

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -3,6 +3,10 @@ require './lib/cookbook'
 RSpec.describe CookBook do
   before :each do
     @cookbook = CookBook.new
+    @ingredient1 = Ingredient.new({name: 'Cheese', unit: 'C', calories: 100})
+    @ingredient2 = Ingredient.new({name: 'Macaroni', unit: 'oz', calories: 30})
+    @recipe1 = Recipe.new('Mac and Cheese')
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   describe '#initialize' do
@@ -12,6 +16,15 @@ RSpec.describe CookBook do
 
     it 'initializes with an empty array of recipes' do
       expect(@cookbook.recipes).to eq []
+    end
+  end
+
+  describe '#add_recipe' do
+    it 'can add recipes to a cookbook' do
+      @cookbook.add_recipe(@recipe1)
+      expect(@cookbook.recipes).to eq [@recipe1]
+      @cookbook.add_recipe(@recipe2)
+      expect(@cookbook.recipes).to eq [@recipe1, @recipe2]
     end
   end
 end
