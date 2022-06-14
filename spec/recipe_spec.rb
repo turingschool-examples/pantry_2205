ea0108d10cf1d8291f15d65cd3177481d2266010
Jared Hardinger <jaredhardinger@gmail.com>
2022-06-14 12:03:28 -0400

    Add total calories test

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe Recipe do
 	before :each do
 		@ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
 		@ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
 	end
 
   it 'exists' do
@@ -28,5 +31,17 @@ RSpec.describe Recipe do
     expect(@recipe1.ingredients_required).to eq({@ingredient1 => 6, @ingredient2 => 8})
 
     expect(@recipe1.ingredients).to eq([@ingredient1, @ingredient2])
+  end
+
+  it 'can count total calories' do
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    expect(@recipe1.total_calories).to eq(440)
+    expect(@recipe2.total_calories).to eq(675)
   end
 end
