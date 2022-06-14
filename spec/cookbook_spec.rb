6b12307875b25123316b0ea4e264758b474bf838
alepbloyd <alexanderleeboyd@gmail.com>
2022-06-14 11:57:11 -0400

    Test: highest_calorie_meal

--- spec/cookbook_spec.rb
+++ spec/cookbook_spec.rb
@@ -10,12 +10,22 @@ RSpec.describe CookBook do
     @ingredient1 = Ingredient.new({
       name: "Cheese",
       unit: "oz",
-      calories: 50})
+      calories: 100})
 
     @ingredient2 = Ingredient.new({
       name: "Macaroni",
       unit: "oz",
-      calories: 200})
+      calories: 30})
+
+    @ingredient3 = Ingredient.new({
+      name: "Ground Beef",
+      unit: "oz",
+      calories: 100})
+
+    @ingredient4 = Ingredient.new({
+      name: "Bun",
+      unit: "g",
+      calories: 75})
 
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
@@ -36,6 +46,20 @@ RSpec.describe CookBook do
     @cookbook.add_recipe(@recipe2)
 
     expect(@cookbook.recipes).to eq([@recipe1,@recipe2])
+  end
+
+  it 'returns highest calorie meal' do
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    expect(@cookbook.highest_calories_meal).to eq(@recipe2)
   end
 
 end
