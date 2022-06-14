508a43e8b0e7df013325fef49aeb57fd5f5f397b
Thiago Silveira <thiago1992@yahoo.com>
2022-06-14 10:03:55 -0600

    Test: Recipe exists

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -2,5 +2,8 @@ require './lib/ingredient'
 require './lib/recipe'
 
 RSpec.describe Recipe do
-  
+  it 'exists' do
+    recipe1 = Recipe.new("Mac and Cheese")
+    expect(recipe1).to be_a(Recipe)
+  end
 end
