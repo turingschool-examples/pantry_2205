a4ce3203075b77e6a2db4307e43f9934c665ac93
Bryce Simonds <bsimonds14@gmail.com>
2022-06-14 10:07:11 -0600

    Feat: add ingredient method complete

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,16 @@
 class Recipe
   attr_reader :name, :ingredients_required
-  
+
   def initialize(name)
     @name = name
     @ingredients_required = Hash.new(0)
+  end
+
+  def add_ingredient(ingredient, quantity)
+    if @ingredients_required.include?(ingredient) == false
+      @ingredients_required[ingredient] = quantity
+    else
+      @ingredients_required[ingredient] += quantity
+    end
   end
 end
